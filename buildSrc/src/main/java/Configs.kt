b6d887617260bc1b9040aa6723de21a1b7d65f60
HoangNH <markharrisons1235@gmail.com>
2025-05-03 15:21:57 +0700

    add buildSrc

---
+++ buildSrc/src/main/java/Configs.kt
@@ -0,0 +1,10 @@
+object Configs {
+    object Dev {
+        const val APPLICATION_ID = "com.baseapp.dev"
+        const val APPLICATION_NAME = "baseapp-DEV"
+    }
+    object RELEASE {
+        const val APPLICATION_ID = "com.baseapp"
+        const val APPLICATION_NAME = "baseapp"
+    }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,12 @@
+object Versions {
+    object App {
+        const val VERSION_CODE = 1
+        const val VERSION_NAME = "1.0"
+    }
+
+    object Android {
+        const val MIN_SDK = 24
+        const val COMPILE_SDK = 34
+        const val TARGET_SDK = 34
+    }
+}
